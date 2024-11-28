02b9f5405c3b050eabea71bc904908a9ebcb57b7
vishaldhakal <vishaldhakal96@gmail.com>
2024-11-28 23:38:30 +0545

    initial

--- sixdesign_be/settings.py
+++ sixdesign_be/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'sixdesign_be.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sixdesign',
+        'USER': 'vishal',
+        'PASSWORD': 'DatabaseUserPassword1!',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

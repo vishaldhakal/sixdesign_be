65fff78b12788ba89ed007adad6bf9421333202e
Naseeb <40313494+naseeb0@users.noreply.github.com>
2024-12-30 17:56:33 +0545

    CorrectedUSer

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from unfold.admin import ModelAdmin
+from django.core.exceptions import ValidationError
 from .models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'role')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exists():
+            raise ValidationError('A user with this email already exists.')
+        return email
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin, ModelAdmin):
+    add_form = CustomUserCreationForm
     list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_active')
     list_filter = ('role', 'is_active', 'is_staff')
     search_fields = ('username', 'email', 'first_name', 'last_name')
@@ -15,4 +29,11 @@ class CustomUserAdmin(UserAdmin, ModelAdmin):
         ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
         ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role', 'first_name', 'last_name'),
+        }),
     )
